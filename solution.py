7d220e7c4c2d81648920fcb3dc7664a6bd975648
HLNSC-MatthewHowells <90606559+HLNSC-MatthewHowells@users.noreply.github.com>
2022-11-16 11:54:31 +0000

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,9 @@
+start
+customer_name = input("Please enter your name")
+address = input("Please enter your address")
+customer_phone_number = input("Please enter your phone number")
+
+width = input("Please enter the width")
+height = input("Please enter the height")
+lawncare = input("Enter the desired lawn care qaulity")
+width * height = area_of_lawn
